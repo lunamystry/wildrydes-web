39368613ed3b38eab8e63d34b64fa88c22d969ad
Mandla Mbuli <leonard.mbuli@entelect.co.za>
2025-07-09 16:29:55 +0200

    Add user pool stuff

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'eu-central-1_32ir2LiN8', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '6619i28g6i54k3rfq1k54ejig3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-central-1' // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
 };
